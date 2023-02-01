14a9914191eae38a105f606302b8433351dd4af2
Mahmoud Nazzal <gmskystar@gmail.com>
2023-02-01 11:27:07 +0400

    asd

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import type { Frame } from 'react-native-vision-camera';
 
 
-export function frameToBase64(frame: Frame,options: {width:number;height:number;quality:number}={width:500,height:500,quality:100}): string {
+export function frameToBase64(frame: Frame, options: any): string {
   'worklet';
   // @ts-ignore
-  return __frameToBase64(frame,options);
+  return __frameToBase64(frame, options);
 }
